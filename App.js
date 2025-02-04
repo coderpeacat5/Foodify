1aa77e8337539f377826219c1e0e1ffe0cc1b0d7
Gayatri <gayatrisingh2300@gmail.com>
2025-02-04 17:50:33 +0530

    second commit

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-const heading = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h2", {}, "I am an h2 tag"),
-        React.createElement("h3", {}, "I am an h3 tag")
-    ]),
-    React.createElement("div", { id: "child2" }, [  // Corrected placement of parenthesis
-        React.createElement("h2", {}, "I am an h2 tag"),
-        React.createElement("h3", {}, "I am an h3 tag")
-    ])
-]);
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+const heading= React.createElement(
+    "h1",
+    {id: "heading"},
+    "This is heading"
+);
+
+// JSX
+const jsxHeading= <h1 id="heading2">Heading using JSX!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
