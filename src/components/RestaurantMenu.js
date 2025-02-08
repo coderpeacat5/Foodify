90e979ddc952f45ed7735e22aa0ca46432be8552
Gayatri <gayatrisingh2300@gmail.com>
2025-02-08 19:37:49 +0530

    create custom hook for modular code

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,26 +1,12 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
-import { MENU_API, MENU_URL } from "../utils/constants";
+import { MENU_URL } from "../utils/constants";
 import { useParams } from "react-router-dom";
 
 const RestaurantMenu = () => {
-    const [resInfo, setResInfo] = useState(null);
-
+    
     const { resId } = useParams();
 
-    useEffect(() => {
-        fetchMenu();
-    }, [])
-
-    const fetchMenu = async () => {
-        const data = await fetch(MENU_API + resId +
-            "&catalog_qa=undefined&submitAction=ENTER"
-        )
-        const json = await data.json();
-        console.log(json);
-        setResInfo(json.data);
-
-    }
+    const resInfo = useRestaurantMenu(resId)
 
     if (resInfo === null) return <Shimmer />
 
